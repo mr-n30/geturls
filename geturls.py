c4fba72564bca7823ccaeb332b47c5828f32250f
mr-n30 <69602501+mr-n30@users.noreply.github.com>
2020-08-24 20:39:20 -0500

    Fixed bugs

--- geturls.py
+++ geturls.py
@@ -13,13 +13,13 @@ parser = argparse.ArgumentParser(description="A Python script to request multipl
 parser.add_argument("-v", "--verbose", type=str, help="Print current HTTP request and response status code", metavar='')
 parser.add_argument("-o", "--out", type=str, help="Directory to store output in", required=True, metavar='')
 parser.add_argument("-H", "--header", type=str, help="HTTP headers to send in the request (key: value) - Multiple uses are allowed", action="append", metavar='')
-parser.add_argument("-t", "--threads", type=int, help="Threads - Default 10", metavar='', default=10)
-parser.add_argument("-f", "--file", type=str, help="File containing list of URLs to fetch", required=True, metavar='')
+parser.add_argument("-t", "--threads", type=int, help="Threads (Default 10)", metavar='', default=10)
+parser.add_argument("-f", "--file", type=str, help="File containing URLs to fetch", required=True, metavar='')
 
 # Parse command line
 args         = parser.parse_args()
 in_file      = args.file
-output_dir     = args.out
+output_dir   = args.out
 verbosity    = args.verbose
 user_header  = args.header
 thread_count = args.threads
@@ -64,14 +64,18 @@ GET URLs
 """
 def get_url(url):
 	try:
-		r = requests.get("http://" + url, timeout=0.500, headers=headers, verify=False)
-		r = requests.get("https://" + url, timeout=0.500, headers=headers, verify=False)
+		r1 = requests.get("http://" + url, timeout=0.500, headers=headers, verify=False)
+		r2 = requests.get("https://" + url, timeout=0.500, headers=headers, verify=False)
 		if args.verbose:
 				print("[+] Trying:\t%s\t%d\t%d" % (url, len(r.text), r.status_code))
-		if r.status_code == 200 or r.status_code == 301 or r.status_code == 302:
-				print("[+] %s\t%d\t%d" % (url, len(r.text), r.status_code))
-		file_name = rand_char_gen()
-		write_to_output_file(file_name, url, str(len(r.text)), str(r.status_code), r.text)
+		if r1.status_code == 200 or r1.status_code == 301 or r1.status_code == 302:
+				print("[+] %s\t%d\t%d" % (url, len(r1.text), r1.status_code))
+		if r2.status_code == 200 or r2.status_code == 301 or r2.status_code == 302:
+				print("[+] %s\t%d\t%d" % (url, len(r2.text), r2.status_code))
+		file_name_1 = rand_char_gen()
+		file_name_2 = rand_char_gen()
+		write_to_output_file(file_name_1, url, str(len(r1.text)), str(r1.status_code), r1.text)
+		write_to_output_file(file_name_2, url, str(len(r2.text)), str(r2.status_code), r2.text)
 	except requests.exceptions.Timeout:
 		pass
 	except requests.exceptions.ConnectionError:
